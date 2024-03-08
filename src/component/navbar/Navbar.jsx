2af05bff2a51fbd623cdeecb294bfbb37d5c1db3
Janamjay <janamjayk6@gmail.com>
2024-03-08 16:12:09 +0530

    done

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   return (
     <div className="bg-black w-full fixed top-0 z-[100]">
-      <div className="flex justify-between">
+      <div className="flex w-full justify-between p-2">
         <div className="px-[3rem]  flex h-[59.5px] items-center justify-between">
           <div className="flex items-center gap-5">
             <div className="flex flex-col items-center justify-center">
@@ -18,21 +18,25 @@ const Navbar = () => {
               </p>
             </div>
             <div className="flex bg-[#171738] w-[91px] h-[33px] justify-center items-center gap-2 rounded-[6.25px]">
-              <img src={flagIcon} alt="" className="w-[40px] rounded-[6.25px] " />
+              <img
+                src={flagIcon}
+                alt=""
+                className="w-[40px] rounded-[6.25px] "
+              />
               <p className="text-white text-base mt-[-2px]">UAE</p>
             </div>
           </div>
-          <div className="hidden md:block">
-            <ul className="text-white flex items-center gap-[40px] font-[400] text-[15px]">
-              <li>About Us</li>
-              <li>Shop</li>
-              <li>Refer & Earn</li>
-              <li>Contact Us</li>
-              <button className="font-[600] text-[12px] w-[158px] h-[45px] rounded-[5px] border-[1px] border-[#453E75]">
-                Sign In/ Sign Up
-              </button>
-            </ul>
-          </div>
+        </div>
+        <div className="hidden md:block">
+          <ul className="text-white flex items-center gap-[40px] font-[400] text-[15px]">
+            <li>About Us</li>
+            <li>Shop</li>
+            <li>Refer & Earn</li>
+            <li>Contact Us</li>
+            <button className="font-[600] text-[12px] w-[158px] h-[45px] rounded-[5px] border-[1px] border-[#453E75]">
+              Sign In/ Sign Up
+            </button>
+          </ul>
         </div>
         <div className="block md:hidden mr-[50px]">
           <div className="flex items-center h-full">
